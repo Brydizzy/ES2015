8badfc477eae210c2a06d2eb800fb90a85c5cb4f
Brydizzy <107905417+Brydizzy@users.noreply.github.com>
2024-04-23 19:59:46 -0700

    Add files via upload

---
+++ es2015.js
@@ -0,0 +1,5 @@
+QUIZ
+1) can reassign var but cannot redeclare with let
+2) difference between var and const is that you can reassign and redeclare var but you cant do that with const 
+3) can reassign let but ca redeclare const 
+Hositing is when you canm access before they are defined in code base
